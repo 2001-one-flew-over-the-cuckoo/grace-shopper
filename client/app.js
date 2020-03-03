dfdfa5f844aa80f4b6bfdd010a8444642cd80859
Katherine Santiago <santiago.kat@gmail.com>
2020-03-03 16:38:32 -0500

    added reducer thunk and action for all products

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
 import SingleProduct from './components/SingleProduct'
+import Products from './components/products'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Route path="/products" component={Products} />
       <Route path="/products/:productId" component={SingleProduct} />
       <Routes />
     </div>
---
+++ client/components/products.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Products extends React.Component {
+  render() {
+    return <div>All Products Component</div>
+  }
+}
+
+export default Products
