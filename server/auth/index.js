63c3be304c5f16aaf5aa15a0ea3775a13cd29ee7
Katherine Santiago <santiago.kat@gmail.com>
2020-03-08 19:23:47 -0400

    chore: added a temp fix to user.orders issue for the cart

--- server/auth/index.js
+++ server/auth/index.js
@@ -45,7 +45,8 @@ router.post('/signup', async (req, res, next) => {
     if (req.body.email) userInfo.email = req.body.email
     if (req.body.password) userInfo.password = req.body.password
     const user = await User.create(userInfo)
-
+    // i'm forcing the user object that we're passing to res.json to have an orders array here but its not saving it in the database. we want to give a newly signed up user an orders array so that the cart info wont break on render
+    user.orders = []
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
