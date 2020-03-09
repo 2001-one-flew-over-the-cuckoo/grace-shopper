28516bea9a75b39d379700d1fcddb404d6995e8d
Katherine Santiago <santiago.kat@gmail.com>
2020-03-08 22:10:26 -0400

    create and associate new order to new user

--- client/components/cart.js
+++ client/components/cart.js
@@ -5,7 +5,6 @@ import {me, removeProductFromCart} from '../store/user'
 import {useHistory} from 'react-router-dom'
 
 const Cart = props => {
-  // console.log(props)
   let history = useHistory()
   const handleDeleteClick = event => {
     event.preventDefault()
@@ -15,40 +14,45 @@ const Cart = props => {
     event.preventDefault()
     history.push('/checkout')
   }
-  const cart = props.user.orders.find(order => order.completed === false)
-  if (cart.products.length > 0) {
-    let cart = props.user.orders[0].products
-    return (
-      <div>
-        {props.user.email}
-        {cart.map(prodInCart => {
-          return (
-            <div key={prodInCart.id} id="prodInCart">
-              <img src={prodInCart.image} />
-              <div>{prodInCart.name}</div>
-              <div>Quantity (drop-down to be added)</div>
-              <div>{(prodInCart.price / 100).toFixed(2)}</div>
-              <button onClick={handleDeleteClick} id={prodInCart.id}>
-                [x]
-              </button>
-            </div>
-          )
-        })}
-        <div id="subtotal">
-          <div>Subtotal</div>
-          <div>
-            {(
-              cart.reduce((acc, currVal) => {
-                return acc + currVal.price
-              }, 0) / 100
-            ).toFixed(2)}
-          </div>
-        </div>
-        <button onClick={checkoutHandler}>Checkout</button>
-      </div>
-    )
-  } else {
+  console.log(props.user)
+  if (props.user.orders === undefined) {
     return <div>You have no items in your cart.</div>
+  } else {
+    const cart = props.user.orders.find(order => order.completed === false)
+    if (cart.products.length > 0) {
+      let cart = props.user.orders[0].products
+      return (
+        <div>
+          {props.user.email}
+          {cart.map(prodInCart => {
+            return (
+              <div key={prodInCart.id} id="prodInCart">
+                <img src={prodInCart.image} />
+                <div>{prodInCart.name}</div>
+                <div>Quantity (drop-down to be added)</div>
+                <div>{(prodInCart.price / 100).toFixed(2)}</div>
+                <button onClick={handleDeleteClick} id={prodInCart.id}>
+                  [x]
+                </button>
+              </div>
+            )
+          })}
+          <div id="subtotal">
+            <div>Subtotal</div>
+            <div>
+              {(
+                cart.reduce((acc, currVal) => {
+                  return acc + currVal.price
+                }, 0) / 100
+              ).toFixed(2)}
+            </div>
+          </div>
+          <button onClick={checkoutHandler}>Checkout</button>
+        </div>
+      )
+    } else {
+      return <div>You have no items in your cart.</div>
+    }
   }
 }
 
