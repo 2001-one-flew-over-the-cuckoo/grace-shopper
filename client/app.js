43800421efc48826cdf2e74a637a49f4d36ded58
Katherine Santiago <santiago.kat@gmail.com>
2020-03-10 19:33:15 -0400

    fix: added a home component and created path to / for that component

--- client/app.js
+++ client/app.js
@@ -4,16 +4,12 @@ import Routes from './routes'
 import SingleProduct from './components/SingleProduct'
 import AllProducts from './components/AllProducts'
 import ManageProducts from './components/admin-components/manage-products'
-import HomeHero from './components/home-hero'
-import HomeProducts from './components/home-products'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <HomeHero />
-      <HomeProducts />
       <Routes />
     </div>
   )
