516691a554de81bd48fda9df1dea9f506777f548
Katherine Santiago <santiago.kat@gmail.com>
2020-03-11 08:26:35 -0400

    fix: shopping cart now shows up in nav even if not logged in

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -27,11 +27,11 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">
-            <FontAwesomeIcon icon={faShoppingCart} />
-          </Link>
         </span>
       )}
+      <Link to="/cart">
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </Link>
     </nav>
   </div>
 )
